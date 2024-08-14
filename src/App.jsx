515161f43f1883eb925ce059f293c288049b3306
Manuel <manuelloverso60@gmail.com>
2024-08-14 17:52:27 +0200

    add star-rating component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import AppHeader from "./components/AppHeader";
+import StarRating from "./components/StarRating";
 
 export default function App() {
-  return <AppHeader />;
+  return (
+    <>
+      <AppHeader />
+      <StarRating />
+    </>
+  );
 }
